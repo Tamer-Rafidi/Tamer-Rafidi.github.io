0cb8f856089bc55ea77f68abfd6d315c90974c8f
tamerrafidi <90111378+tamerrafidi@users.noreply.github.com>
2023-12-08 22:37:01 -0500

    Added scroll up button

--- myscripts.js
+++ myscripts.js
@@ -27,12 +27,21 @@ document
     toggleModal(".follower-modal", false);
 });
 
-document.getElementById("following").addEventListener("click", function () {
+document.getElementById("following").addEventListener("click", function() {
   toggleModal(".following-modal", true);
 });
 
-document
-  .querySelector(".following-close")
-  .addEventListener("click", function () {
+document.querySelector(".following-close").addEventListener("click", function () {
     toggleModal(".following-modal", false);
   });
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
+
