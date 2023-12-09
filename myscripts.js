0cd96a430dba68bc439a49b096b600d9387043fb
tamerrafidi <90111378+tamerrafidi@users.noreply.github.com>
2023-12-08 22:26:06 -0500

    Finished "following" and "followers" tab minus the icons

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,38 @@
-document.getElementById('cta').addEventListener('click', function(){
-  document.querySelector('.modal').style.display = 'flex';
+function toggleModal(modalSelector, shouldOpen) {
+  var modal = document.querySelector(modalSelector);
+  if (shouldOpen) {
+    modal.style.display = "flex";
+    document.body.style.overflow = "hidden";
+  } else {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto";
+  }
+}
+
+document.getElementById("cta").addEventListener("click", function () {
+  toggleModal(".modal", true);
 });
 
-document.querySelector('.close').addEventListener('click', function(){
-  document.querySelector('.modal').style.display = 'none';
+document.querySelector(".close").addEventListener("click", function () {
+  toggleModal(".modal", false);
 });
 
+document.getElementById("follower").addEventListener("click", function () {
+  toggleModal(".follower-modal", true);
+});
+
+document
+  .querySelector(".follower-close")
+  .addEventListener("click", function () {
+    toggleModal(".follower-modal", false);
+});
+
+document.getElementById("following").addEventListener("click", function () {
+  toggleModal(".following-modal", true);
+});
+
+document
+  .querySelector(".following-close")
+  .addEventListener("click", function () {
+    toggleModal(".following-modal", false);
+  });
